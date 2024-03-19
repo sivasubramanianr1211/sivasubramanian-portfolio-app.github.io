53ddf704dd399a4de51c57835b5c4c405b30c29a
Siva Subramanian <sramiah@forbesadvisor.com>
2024-03-19 19:29:38 +0530

    adding bug fixes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,12 +2,8 @@
 
 import React from "react";
 import ProfileImg from "../assets/profile-img.jpg";
-import useWindowSize from "../utils/useWindowSize";
 
 const Profile: React.FC = () => {
-  const { width: windowWidth } = useWindowSize();
-  console.log(windowWidth);
-  const isMobile: boolean = windowWidth !== undefined && windowWidth < 768;
   return (
     <div className="items-center m-0 p-[20px]">
       <div className="flex justify-center">
@@ -17,6 +13,7 @@ const Profile: React.FC = () => {
             height={"200px"}
             width={"200px"}
             src={ProfileImg}
+            alt=""
           />
         </div>
       </div>
